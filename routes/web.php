d53840f07e6180b9f47f17b8d2d8c992d0fd7dc0
Nafi <54702140+haynafi@users.noreply.github.com>
2025-05-04 18:42:01 +0700

    Fix: / routes changes to dashboard not template

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/pembayaran', function () {
+//     return view('pembayaran');
+// })->middleware(['auth', 'verified'])->name('pembayaran');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
