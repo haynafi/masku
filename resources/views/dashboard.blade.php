5eb3e120b90d483d43cb1948360227accb890bce
Nafi <54702140+haynafi@users.noreply.github.com>
2025-05-04 17:40:07 +0700

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="bg-white p-6 rounded-xl shadow">
+        <h2 class="text-xl font-semibold mb-2">Selamat datang di MASKU</h2>
+        <p class="text-gray-600">Ini adalah halaman utama untuk mencatat transaksi emas.</p>
+    </div>
+@endsection
